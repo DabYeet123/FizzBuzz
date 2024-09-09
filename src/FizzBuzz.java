7a6a53f4f042490a1b28c06d0238e33775a1d714
zeyu cai <zeyucai123456789@gmail.com>
2024-09-09 18:31:32 -0400

    Edit Read

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -13,7 +13,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doBuzz(int i) {
+    public static int doBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
