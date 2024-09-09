7286192504b1f684437cdd3b7dc9eb504e75b1db
zeyu cai <zeyucai123456789@gmail.com>
2024-09-09 18:27:20 -0400

    Edit Read

---
+++ src/Multiples.java
@@ -0,0 +1,15 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 3;
+        int counter = 0;
+        while(i < 1000) {
+            if(i % 3 == 0) {
+                counter++;
+            }else if(i % 5 == 0){
+                counter++;
+            }
+            i++;
+        }
+        System.out.println(counter);
+    }
+}
